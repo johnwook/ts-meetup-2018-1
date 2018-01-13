4676f1073ce2dd359fd25b4c835f007cdbfbb235
Johnwook Choi <johnwook.choi@huiseoul.com>
2018-01-13 17:15:55 +0900

    antd 사용 component 예시 추가
    - global style 적용을 위한 css 추가
    - next.js 에서는 css load 가 어렵기 때문입니다

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,16 @@
-export default () => <div>Welcome to next.js!</div>
+import { Button } from "antd";
+import Head from "next/head";
+
+export default () => (
+  <div>
+    <Head>
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.1.1/antd.min.css"
+      />
+    </Head>
+    Welcome to next.js!
+    <Button>Antd</Button>
+  </div>
+);
