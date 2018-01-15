7d70e7f26e40d71eb7420f7b427d1c3d161adcc2
Johnwook Choi <johnwook.choi@huiseoul.com>
2018-01-15 23:45:00 +0900

    DefaultHead 로 추출
    - antd css 가 계속 보일 필요가 없기 때문입니다

---
+++ common/DefaultHead.js
@@ -0,0 +1,5 @@
+import Head from "next/head";
+const DefaultHead = () => (<Head>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.1.1/antd.min.css"/>
+  </Head>);
+export default DefaultHead;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,10 @@
 import { Button } from "antd";
-import Head from "next/head";
+
+import DefaultHead from "../common/DefaultHead";
 
 export default () => (
   <div>
-    <Head>
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.1.1/antd.min.css"
-      />
-    </Head>
+    <DefaultHead />
     Welcome to next.js!
     <Button>Antd</Button>
   </div>
