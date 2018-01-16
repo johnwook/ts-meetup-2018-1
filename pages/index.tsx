0ae561c18ae8214b2f51cc17b7127535752db74b
Johnwook Choi <johnwook.choi@gmail.com>
2018-01-16 14:35:59 +0900

    static markup
    - requirements 정리에 앞서 우선 그려봤습니다.
    - 이제 requirements 를 정리해야...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,44 @@
-import { Button } from "antd";
+import { Card, Icon } from "antd";
 
 import DefaultHead from "../common/DefaultHead";
 
 export default () => (
   <div>
     <DefaultHead />
-    Welcome to next.js!
-    <Button>Antd</Button>
+    <Card
+      actions={[
+        <Icon key="retweet" type="retweet" />,
+        <Icon key="delete" type="delete" />
+      ]}
+      title="Johnwook Choi"
+      extra={
+        <div>
+          @johnwook <Icon type="retweet" />
+        </div>
+      }
+      style={{ width: 400 }}
+    >
+      <p>
+        We're higing 지구에서 처음 구매자와 판매자 사이에 거래가 생긴 날 이래,
+        사람이 사람에게 물건을 산다는 사실은 바뀌지 않았습니다. 바뀌지 않는
+        사실위에 기술이라는 날개를 다는 것이 우리가 하는 일입니다.
+      </p>
+    </Card>
+    <Card
+      actions={[
+        <Icon key="retweet" type="retweet" />,
+        <Icon key="delete" type="delete" />
+      ]}
+      title="TypeScript Korea"
+      extra="@tskorea"
+      style={{ width: 400 }}
+    >
+      <p>
+        안녕하세요, TypeScript Korea 운영진입니다. 많은 개발자 분들의
+        TypeScript에 대한 관심을 등에 업고 TypeScript Korea에서 Meetup을
+        준비했습니다. 본 행사는 TypeScript Korea 운영진들이 자발적으로 진행하는
+        비영리 행사입니다.
+      </p>
+    </Card>
   </div>
 );
